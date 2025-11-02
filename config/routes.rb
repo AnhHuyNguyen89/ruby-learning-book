d3a143677f7ce438c9f0dbc15c7409741c9293ba
Anh Huy Nguyen <64916063+AnhHuyNguyen89@users.noreply.github.com>
2025-11-02 20:07:13 +1030

    commit 3rd time

---
+++ config/routes.rb
@@ -0,0 +1,49 @@
+Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
+  get "sessions/new"
+  get "web_base_users/new"
+  get "user/new"
+  root "static_pages#home"
+  # header navigation
+  get "/home", to: "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+  # get "/user", to: "users#create"
+  # post "/user", to: "users#create"
+
+  # login
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
+
+  # resources
+  resources :web_base_users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [ :edit ]
+  resources :password_resets, only: [ :new, :create, :edit, :update ] # new_password_reset_path
+  resources :microposts, only: [ :create, :destroy ]
+  resources :relationships, only: [ :create, :destroy ]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  # root "application#hello"
+  # root "users#index"
+  # root "microposts#index"
+end
